96531ce4d8052c1eac69fe6c81abdf4298a547e7
gigaherz <gigaherz@04eee4ca-fda2-11de-b5bc-ffd2847a4b57>
2010-01-10 05:04:07 +0000

    Branch alpha 1 release.

---
+++ branches/alpha1/resource.h
@@ -0,0 +1,34 @@
+//FMVTool2: The FMV Extractor (v2)
+//Copyright (C) 2010  David Quintana <gigaherz@gmail.com>
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FMVTool2.rc
+//
+#define IDB_PNG1                        101
+#define IDB_LOGO                        101
+#define IDB_BITMAP2                     106
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ branches/alpha1/stdafx.h
@@ -0,0 +1,45 @@
+//FMVTool2: The FMV Extractor (v2)
+//Copyright (C) 2010  David Quintana <gigaherz@gmail.com>
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#endif						
+
+#include <windows.h>
+#include <tchar.h>
+
+// TODO: reference additional headers your program requires here
+
+#undef min
+#undef max
+
+class Process;
+
+struct StreamItem
+{
+	int sector;
+	int offset;
+	int length;
+};
+
+#include <wx/wx.h>
